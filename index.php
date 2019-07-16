ddb4aafbec03ee80e93ea0d54e8829eae7343bd8
Shion <a951753abc@gmail.com>
2019-07-16 15:09:36 +0800

    Add change event

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
     <title>時よ止まれ</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script>
+        $('#s1').on('change', function() {
+            console.log('.on(change) = ' + $(this).val());
+        });
         function nameChange() {
             $.getJSON('backend.php', function(data) {
                 $.each(data, function(name, values) {
@@ -25,6 +28,6 @@
 雖然經過兩個禮拜的修養，HP值已經恢復八成了，但是和其他玩家相比，我的HP還不是能進行戰鬥狀況。 <br>
 將骰子召喚出來後握在手上，我等待著清水的攻擊，而清水也沒有辜負我的期待，他像變魔術一樣，憑空變出兩顆圓球狀的物體後，冷冷的從嘴裡說出了這場戰鬥的啟動語。 <br>
 「使用投擲。」 <br>
-「 <label><input type="text" title="serifu"></label> 」 <br>
+「 <label><input id="s1" type="text" title="serifu"></label> 」 <br>
 </body>
 </html>

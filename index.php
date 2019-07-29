fe7229131ad81bf65a0b168394cea28f501aa8b0
Shion <a951753abc@gmail.com>
2019-07-30 00:57:54 +0800

    Modify change to easy way

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+$novel = ['霞之謎', 'Gold/Apocrypha -秋雨新霽-'];
+$num = array_rand($novel, 1);
+$text = '';
+switch ($num) {
+    case 0:
+        $novel_tail = file_get_contents('novel_tail_1.html');
+        break;
+    case 1:
+        $novel_tail = file_get_contents('novel_tail_2.html');
+        break;
+}
+?>
 <html lang="zh_TW">
 <head>
     <title>時よ止まれ</title>
@@ -7,13 +20,7 @@
             // $('#s1').on('change', function() {
             //     console.log('.on(change) = ' + $(this).val());
             // });
-            novelTail();
         });
-        function novelTail() {
-            $.getJSON('novelTail.php', function(data) {
-                $('#novel-tail').text(data);
-            });
-        }
         function nameChange() {
             $.getJSON('backend.php', function(data) {
                 $.each(data, function(name, values) {
@@ -37,8 +44,7 @@
 認識他以來，每回都可以看到他在閱讀不同種類的書籍。 <br>
 「接龍。小說接龍。由數名作者在幾乎無規則下的環境書寫故事，閱讀時可以從各種角度切入分析，挺有意思的。」 <br>
 說著說著他把書本刻意抬高，使我能看到上頭的書名。 <br>
-<span id="novel-tail"></span>
+<span id="novel-tail"><?=$novel_tail?></span>
 <!--「 <label><input id="s1" type="text" title="serifu"></label> 」 <br>-->
-
 </body>
 </html>

25a9da4c94e2e87fb67e6ccfa580c14e5ae0bffc
Shion <a951753abc@gmail.com>
2019-07-30 23:02:49 +0800

    Modify css

--- index.php
+++ index.php
@@ -22,6 +22,17 @@ switch ($num) {
             font-size: 16px;
             font-weight: 400;
             line-height: 1.75;
+            font-variant-ligatures: none;
+            margin: 0;
+            outline: none;
+            position: relative;
+        }
+        p {
+            display: block;
+            margin-block-start: 1em;
+            margin-block-end: 1em;
+            margin-inline-start: 0px;
+            margin-inline-end: 0px;
         }
     </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -50,11 +61,11 @@ switch ($num) {
 </head>
 <body>
 <div class="mGzaTb">
-    「這次的合作夥伴嗎......是你啊。上回是園藝，這回又在看什麼？」 <br>
-    來到集合地點後，我打量著穿著深褐色皮衣外套，有著亂糟糟鳥窩頭，坐在地板上讀書的男人。 <br>
-    認識他以來，每回都可以看到他在閱讀不同種類的書籍。 <br>
-    「接龍。小說接龍。由數名作者在幾乎無規則下的環境書寫故事，閱讀時可以從各種角度切入分析，挺有意思的。」 <br>
-    說著說著他把書本刻意抬高，使我能看到上頭的書名。 <br>
+    <p>「這次的合作夥伴嗎......是你啊。上回是園藝，這回又在看什麼？」</p>
+    <p>來到集合地點後，我打量著穿著深褐色皮衣外套，有著亂糟糟鳥窩頭，坐在地板上讀書的男人。</p>
+    <p>認識他以來，每回都可以看到他在閱讀不同種類的書籍。</p>
+    <p>「接龍。小說接龍。由數名作者在幾乎無規則下的環境書寫故事，閱讀時可以從各種角度切入分析，挺有意思的。」</p>
+    <p>說著說著他把書本刻意抬高，使我能看到上頭的書名。</p>
     <span id="novel-tail"><?=$novel_tail?></span>
 </div>
 

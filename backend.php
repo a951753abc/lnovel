77956bb4bd9d8d47b8b1a02407b79aef23fe2d28
Shion <a951753abc@gmail.com>
2019-08-06 15:53:10 +0800

    Fix Cover

--- backend.php
+++ backend.php
@@ -1,117 +1,19 @@
 <?php
-if (empty($_COOKIE['loopCount'])) {
-    exit;
-}
-$loopCount = $_COOKIE['loopCount'];
-$hour = date('H');
-if ($hour >= 8) {
-    $root = 1;
+/**
+ * Your file description
+ *
+ * @version 0.1.0
+ * @author ying-huei
+ * @date 2019/7/16
+ * @since 2019/7/16 description
+ */
+if (isset($_COOKIE['loopCount'])) {
+    $loopCount = $_COOKIE['loopCount'];
 } else {
-    $root = 2;
+    setcookie('loopCount', 0, time() + 60 * 60 * 24);
+    $loopCount = $_COOKIE['loopCount'];
 }
-if ($_SERVER['REMOTE_ADDR'] == '111.251.159.104') {
-    $root = 2;
-}
-$edit = false;
-if (($loopCount > 1 && $root == 2) || ($loopCount == 0 && $root == 2)) {
-    $edit = true;
-}
-if (!$edit) {
-    exit;
-}
-?>
-<html lang="zh_TW">
-<head>
-    <title>時よ止まれ</title>
-    <style>
-        .mGzaTb {
-            padding: 8px;
-            color: rgba(33, 33, 33, 1);
-            font-family: "Source Sans Pro", sans-serif;
-            font-size: 16px;
-            font-weight: 400;
-            line-height: 1.75;
-            font-variant-ligatures: none;
-            margin: 0;
-            outline: none;
-            position: relative;
-        }
-
-        p {
-            display: block;
-            margin-block-start: 1em;
-            margin-block-end: 1em;
-            margin-inline-start: 0px;
-            margin-inline-end: 0px;
-        }
-        .myButton {
-            -moz-box-shadow:inset 0px 39px 0px -24px #e67a73;
-            -webkit-box-shadow:inset 0px 39px 0px -24px #e67a73;
-            box-shadow:inset 0px 39px 0px -24px #e67a73;
-            background-color:#e4685d;
-            -moz-border-radius:4px;
-            -webkit-border-radius:4px;
-            border-radius:4px;
-            border:1px solid #ffffff;
-            display:inline-block;
-            cursor:pointer;
-            color:#ffffff;
-            font-family:"Source Sans Pro", sans-serif;
-            font-size:15px;
-            padding:6px 15px;
-            text-decoration:none;
-            text-shadow:0px 1px 0px #734e4b;
-        }
-        .myButton:hover {
-            background-color:#eb675e;
-        }
-        .myButton:active {
-            position:relative;
-            top:1px;
-        }
-    </style>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script>
-        $(function () {
-            ShowTime();
-        });
-
-        function ShowTime() {
-            var NowDate = new Date();
-            var h = NowDate.getHours();
-            var m = NowDate.getMinutes();
-            var s = NowDate.getSeconds();
-            document.getElementById('show-box').innerHTML = h + '時' + m + '分' + s + '秒';
-            setTimeout('ShowTime()', 1000);
-        }
-
-        function changeFate() {
-            $('html,body').animate({scrollTop:0}, 333);
-            window.location.href = 'https://yumehiru.link/ln/backend.php';
-        }
-
-        function metaBreak() {
-            $('html,body').animate({scrollTop:0}, 333);
-            window.location.href = 'https://yumehiru.link/ln/metabreak.php';
-        }
-    </script>
-</head>
-<body>
-<div class="mGzaTb">
-    <p>靠著手電筒微弱的光源，我好不容易走出樹林，來到一塊空地。</p>
-    <p>要說異樣的話，就是不遠處有一間小木屋，以及眼前不到十公尺的距離有一位臉色蒼白的男子，透過手電筒的光源更顯陰森嚇人。</p>
-    <p>「呃、晚安？」</p>
-    <p>我試著以友善的態度對他揮手。</p>
-    <p>「滾回去。」</p>
-    <p>男子厲聲道。</p>
-    <p>「別這麼說嘛。同為能力者也許我們可以聊聊，做個朋友之類的？」</p>
-    <p>聽到能力者三個字，他的表情更加猙獰，隨即像是發現什麼一樣大聲咆嘯：</p>
-    <p>「你是......諾斯底的......走狗！」</p>
-    <p>奇怪我明明沒有自報家門啊？難道是我身上有什麼特徵，例如身上的教袍......好吧。</p>
-    <p>察覺自己的愚蠢後，我高舉雙手，持續釋出善意。</p>
-    <p>「等等，我不是來跟你相殺的。我只是想確認一些事，例如高尾山下的村民遭到洗劫──」</p>
-    <p>他卻不理會我的話語，一個箭步朝我衝來，手電筒的光源也同時消失。</p>
-    <p><br></p>
-</div>
-</body>
-</html>
+$loopCount++;
+setcookie('loopCount', $loopCount, time() + 60 * 60 * 24);
+header("Location: https://yumehiru.link/ln/");
+exit;
